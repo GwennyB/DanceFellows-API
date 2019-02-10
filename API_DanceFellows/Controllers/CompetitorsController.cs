7fab04f08e39f9266b173c759906d2770cdd04f6
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-02-10 11:05:37 -0800

    removed extraneous comments, methods, and notes

--- API_DanceFellows/Controllers/CompetitorsController.cs
+++ API_DanceFellows/Controllers/CompetitorsController.cs
@@ -19,8 +19,12 @@ namespace API_DanceFellows.Controllers
             _context = context;
         }
 
-        [HttpGet()]
-        [Route("Index")]
+        /// <summary>
+        /// default greeting route
+        /// </summary>
+        /// <returns> Hello World </returns>
+        [HttpGet]
+        [Route("index.html")]
         public ActionResult<string> Index()
         {
             return "Hello World";
@@ -32,7 +36,7 @@ namespace API_DanceFellows.Controllers
         /// </summary>
         /// <returns>If a competitor with the given id exists, return the competitor object with a 200 status code. If it does not, return a null object with a 400 status code.</returns>
         [HttpGet("{id}")]
-        [Route("GetCompetitor")]
+        [Route("Competitors/GetCompetitor")]
         // removed ("id") to make this work
         public async Task<Competitor> GetCompetitor(int id)
         {
@@ -43,57 +47,8 @@ namespace API_DanceFellows.Controllers
             {
                 Response.StatusCode = 400;
             }
-            //TODO: how should the API respond to a request with an invalid ID? Currently sending back a null object with 400 status code 
             return competitor;
         }
-
-
-        /// <summary>
-        /// POST: Competitors/RefreshAll
-        /// calls for table dump and subsequent re-populate from external data
-        /// </summary>
-        /// <returns> completion report string </returns>
-        [HttpGet]
-        [Route("RefreshAll")]
-        public async Task<string> RefreshAll()
-        {
-            int count = await _context.RefreshAll();
-            return $"Refresh complete! Added {count} dancer records.";
-        }
-
-        ///// <summary>
-        ///// GET: Competitors/RefreshExternalData
-        ///// Scrapes all competitors data from external API
-        ///// </summary>
-        ///// <returns> dancer data </returns>
-        //[HttpGet]
-        //public async Task<string> RefreshExternalData(int start, int end)
-        //{
-        //    for (int i = 0; i < 18500; i+=250)
-        //    {
-        //        await _context.GetExternalData(i+1, i+250);
-        //    }
-
-        //    return "Refresh complete!";
-        //}
-
-        ///// <summary>
-        ///// GET: api/Competitors (note no id appended to the end)
-        ///// With no id specified, hitting the Get route on Competitors should return all competitors we have stored in the database
-        ///// </summary>
-        ///// <returns>A list of all competitors.</returns>
-        //[HttpGet]
-        //public async Task<IEnumerable<Competitor>> GetAllCompetitors()
-        //{
-        //    IEnumerable<Competitor> allCompetitors = await _context.GetAllCompetitors();
-        //    //Response.StatusCode tip courtesy of https://stackoverflow.com/questions/5072804/how-to-return-a-200-http-status-code-from-asp-net-mvc-3-controller
-        //    Response.StatusCode = 200;
-        //    if (allCompetitors == null)
-        //    {
-        //        Response.StatusCode = 500;
-        //    }
-        //    return allCompetitors;
-        //}
 
     }
 }
