3aa6b1c2e64da4c1f39153648c553d379c73d3b8
Blaise Clarke <blaiserclarke@gmail.com>
2019-02-10 13:27:38 -0800

    Added basic get/set tests for Result

--- UnitTests/APIGetSetUnitTests.cs
+++ UnitTests/APIGetSetUnitTests.cs
@@ -102,5 +102,171 @@ namespace API_UnitTests
             comp.MaxLevel = Level.Novice;
             Assert.True(comp.MaxLevel == Level.Novice);
         }
+        //Result
+        [Fact]
+        public void CanGetPlacementOfResult()
+        {
+            Result result = new Result();
+            result.Placement = Placement.Finalled;
+            Assert.True(result.Placement == Placement.Finalled);
+        }
+        [Fact]
+        public void CanSetPlacementOfResult()
+        {
+            Result result = new Result();
+            result.Placement = Placement.Position2;
+            result.Placement = Placement.Finalled;
+            Assert.True(result.Placement == Placement.Finalled);
+        }
+        [Fact]
+        public void CanGetRoleOfResult()
+        {
+            Result result = new Result();
+            result.Role = Role.Follow;
+            Assert.True(result.Role == Role.Follow);
+        }
+        [Fact]
+        public void CanSetRoleOfResult()
+        {
+            Result result = new Result();
+            result.Role = Role.Lead;
+            result.Role = Role.Follow;
+            Assert.True(result.Role == Role.Follow);
+        }
+        [Fact]
+        public void CanGetChiefScoreInResult()
+        {
+            Result result = new Result();
+            result.ScoreChief = 1;
+            Assert.True(result.ScoreChief == 1);
+        }
+        [Fact]
+        public void CanSetChiefScoreInResult()
+        {
+            Result result = new Result();
+            result.ScoreChief = 7;
+            result.ScoreChief = 1;
+            Assert.True(result.ScoreChief == 1);
+        }
+        [Fact]
+        public void CanGetScoreOneInResult()
+        {
+            Result result = new Result();
+            result.ScoreOne = 1;
+            Assert.True(result.ScoreOne == 1);
+        }
+        [Fact]
+        public void CanSetScoreOneInResult()
+        {
+            Result result = new Result();
+            result.ScoreOne = 7;
+            result.ScoreOne = 1;
+            Assert.True(result.ScoreOne == 1);
+        }
+        [Fact]
+        public void CanGetScoreTwoInResult()
+        {
+            Result result = new Result();
+            result.ScoreTwo = 1;
+            Assert.True(result.ScoreTwo == 1);
+        }
+        [Fact]
+        public void CanSetScoreTwoInResult()
+        {
+            Result result = new Result();
+            result.ScoreTwo = 7;
+            result.ScoreTwo = 1;
+            Assert.True(result.ScoreTwo == 1);
+        }
+        [Fact]
+        public void CanGetScoreThreeInResult()
+        {
+            Result result = new Result();
+            result.ScoreThree = 1;
+            Assert.True(result.ScoreThree == 1);
+        }
+        [Fact]
+        public void CanSetScoreThreeInResult()
+        {
+            Result result = new Result();
+            result.ScoreThree = 7;
+            result.ScoreThree = 1;
+            Assert.True(result.ScoreThree == 1);
+        }
+        [Fact]
+        public void CanGetScoreFourInResult()
+        {
+            Result result = new Result();
+            result.ScoreFour = 1;
+            Assert.True(result.ScoreFour == 1);
+        }
+        [Fact]
+        public void CanSetScoreFourInResult()
+        {
+            Result result = new Result();
+            result.ScoreFour = 7;
+            result.ScoreFour = 1;
+            Assert.True(result.ScoreFour == 1);
+        }
+        [Fact]
+        public void CanGetScoreFiveInResult()
+        {
+            Result result = new Result();
+            result.ScoreFive = 1;
+            Assert.True(result.ScoreFive == 1);
+        }
+        [Fact]
+        public void CanSetScoreFiveInResult()
+        {
+            Result result = new Result();
+            result.ScoreFive = 7;
+            result.ScoreFive = 1;
+            Assert.True(result.ScoreFive == 1);
+        }
+        [Fact]
+        public void CanGetScoreSixInResult()
+        {
+            Result result = new Result();
+            result.ScoreSix = 1;
+            Assert.True(result.ScoreSix == 1);
+        }
+        [Fact]
+        public void CanSetScoreSixInResult()
+        {
+            Result result = new Result();
+            result.ScoreSix = 7;
+            result.ScoreSix = 1;
+            Assert.True(result.ScoreSix == 1);
+        }
+        [Fact]
+        public void CanGetECIDInResult()
+        {
+            Result result = new Result();
+            result.EventCompetitionID = 1;
+            Assert.True(result.EventCompetitionID == 1);
+        }
+        [Fact]
+        public void CanSetECIDInResult()
+        {
+            Result result = new Result();
+            result.EventCompetitionID = 7;
+            result.EventCompetitionID = 1;
+            Assert.True(result.EventCompetitionID == 1);
+        }
+        [Fact]
+        public void CanGetCompIDInResult()
+        {
+            Result result = new Result();
+            result.CompetitorID = 1;
+            Assert.True(result.CompetitorID == 1);
+        }
+        [Fact]
+        public void CanSetCompIDInResult()
+        {
+            Result result = new Result();
+            result.CompetitorID = 7;
+            result.CompetitorID = 1;
+            Assert.True(result.CompetitorID == 1);
+        }
     }
 }
